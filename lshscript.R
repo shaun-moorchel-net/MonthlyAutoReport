57df388e35d67d40c9fa94b8b65acc5b9b15c0f3
Shaun Michel <shaun+development@moorchel.net>
2010-09-02 13:24:01 -0400

    Sample commit

--- lshscript.R
+++ lshscript.R
@@ -2,3 +2,4 @@ vyh <- read.csv("LifeskillsthruJan2010.csv",header=TRUE,sep=",",na.string("N/A")
 dEnrolled <- as.Date(lsh$Enrollment,"%m/%d/%Y")
 dWorkshop <- as.Date(lsh$Workshop,"%m/%d/%Y")
 bAttended <- as.(
+# COMMENTS
